079eda99ba91ec9482054c76c5abfa67abc1369b
fatma çimen <ftmcmn8866@gmail.com>
2022-09-27 14:43:19 +0300

    ftm

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -2,7 +2,9 @@ package utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,17 +13,17 @@ import java.time.Duration;
 
     public abstract class TestBase {
         // abstract yapmamizin sebebi, bu class'dan obje uretilmesinin onune gecmektir.
-        protected WebDriver driver;
-        @Before
-        public void setUp(){
+        protected static WebDriver driver;
+        @BeforeClass
+        public static void setUp(){
             WebDriverManager.chromedriver().setup();
             driver= new ChromeDriver();
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
 
-        @After
-        public void tearDown(){
+        @AfterClass
+        public static void tearDown(){
 
             //driver.quit();
         }
